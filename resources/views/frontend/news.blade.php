4262c7befdfb597c01f57928e77e4ec0c9e23b9b
ChiNV <chinguyenvc@gmail.com>
2025-10-10 20:17:59 +0700

    .

--- resources/views/frontend/news.blade.php
+++ resources/views/frontend/news.blade.php
@@ -10,189 +10,24 @@
         </div>
     </div>      
 
-    <div class="rs-event modify1 orange-color pt-100 pb-100 md-pt-70 md-pb-70">
+    <div class="rs-event modify1 orange-color pt-50 pb-50">
         <div class="container">
             <div class="row">
+                 @foreach($articles as $article)
                 <div class="col-lg-4 mb-30 col-md-6">
                     <div class="event-item">
                         <div class="event-short">
                             <div class="featured-img">
-                                <img src="{{ asset('frontend')}}/assets/images/event/1.jpg" alt="Image">
-                                <div class="dates">
-                                        July 24, 2020  
-                                </div>
+                                <a title="{{ $article->name }}" href="{{ url('chia-se-kien-thuc', $article->url) }}"><img src="{{ viewImage($article->image) }}" alt="{{ $article->name }}" width="393" height="222" /></a>
+                                <div class="dates">{{ $article->created_at->format('d/m/Y') }}</div>
                             </div>
-                            <div class="content-part">
-                                <h4 class="title"><a href="#">Spicy Quince And Cranberry Chutney</a></h4>
-                            <div class="time-sec">
-                                <div class="timesec"><i class="fa fa-clock-o"></i> 11:00 AM -  
-                                03:00 AM </div>
-                                <div class="address"><i class="fa fa-map-o"></i> New Margania</div>
-                            </div>
+                            <div class="content-part d-flex justify-content-center text-center">
+                                <h4 class="title"><a title="{{ $article->name }}" href="{{ url('chia-se-kien-thuc', $article->url) }}">{{ $article->name }}</a></h4>
                             </div> 
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-4 mb-30 col-md-6">
-                    <div class="event-item">
-                        <div class="event-short">
-                            <div class="featured-img">
-                                <img src="{{ asset('frontend')}}/assets/images/event/2.jpg" alt="Image">
-                                <div class="dates">
-                                        July 24, 2020  
-                                </div>
-                            </div>
-                            <div class="content-part">
-                                <h4 class="title"><a href="#">Persim, Pomegran, And Massag Kale Salad</a></h4>
-                            <div class="time-sec">
-                                <div class="timesec"><i class="fa fa-clock-o"></i> 11:00 AM -  
-                                03:00 AM </div>
-                                <div class="address"><i class="fa fa-map-o"></i> New Margania</div>
-                            </div>
-                            </div> 
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 mb-30 col-md-6">
-                    <div class="event-item">
-                        <div class="event-short">
-                            <div class="featured-img">
-                                <img src="{{ asset('frontend')}}/assets/images/event/3.jpg" alt="Image">
-                                <div class="dates">
-                                        July 24, 2020  
-                                </div>
-                            </div>
-                            <div class="content-part">
-                                <h4 class="title"><a href="#">Essential Fall Fruits That Aren’t Apples</a></h4>
-                            <div class="time-sec">
-                                <div class="timesec"><i class="fa fa-clock-o"></i> 11:00 AM -  
-                                03:00 AM </div>
-                                <div class="address"><i class="fa fa-map-o"></i> New Margania</div>
-                            </div>
-                            </div> 
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 mb-30 col-md-6">
-                    <div class="event-item">
-                        <div class="event-short">
-                            <div class="featured-img">
-                                <img src="{{ asset('frontend')}}/assets/images/event/4.jpg" alt="Image">
-                                <div class="dates">
-                                        July 24, 2020  
-                                </div>
-                            </div>
-                            <div class="content-part">
-                                <h4 class="title"><a href="#">Job Seekers From Overcoming Failure</a></h4>
-                            <div class="time-sec">
-                                <div class="timesec"><i class="fa fa-clock-o"></i> 11:00 AM -  
-                                03:00 AM </div>
-                                <div class="address"><i class="fa fa-map-o"></i> New Margania</div>
-                            </div>
-                            </div> 
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 mb-30 col-md-6">
-                    <div class="event-item">
-                        <div class="event-short">
-                            <div class="featured-img">
-                                <img src="{{ asset('frontend')}}/assets/images/event/5.jpg" alt="Image">
-                                <div class="dates">
-                                        July 24, 2020  
-                                </div>
-                            </div>
-                            <div class="content-part">
-                                <h4 class="title"><a href="#">Best Technology Graduation Ceremony.</a></h4>
-                            <div class="time-sec">
-                                <div class="timesec"><i class="fa fa-clock-o"></i> 11:00 AM -  
-                                03:00 AM </div>
-                                <div class="address"><i class="fa fa-map-o"></i> New Margania</div>
-                            </div>
-                            </div> 
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 mb-30 col-md-6">
-                    <div class="event-item">
-                        <div class="event-short">
-                            <div class="featured-img">
-                                <img src="{{ asset('frontend')}}/assets/images/event/6.jpg" alt="Image">
-                                <div class="dates">
-                                        July 24, 2020  
-                                </div>
-                            </div>
-                            <div class="content-part">
-                                <h4 class="title"><a href="#">Educational Technology and Mobile Learning</a></h4>
-                            <div class="time-sec">
-                                <div class="timesec"><i class="fa fa-clock-o"></i> 11:00 AM -  
-                                03:00 AM </div>
-                                <div class="address"><i class="fa fa-map-o"></i> New Margania</div>
-                            </div>
-                            </div> 
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 md-mb-30">
-                    <div class="event-item">
-                        <div class="event-short">
-                            <div class="featured-img">
-                                <img src="{{ asset('frontend')}}/assets/images/event/7.jpg" alt="Image">
-                                <div class="dates">
-                                        July 24, 2020  
-                                </div>
-                            </div>
-                            <div class="content-part">
-                                <h4 class="title"><a href="#">Job Seekers From Overcoming Failure</a></h4>
-                            <div class="time-sec">
-                                <div class="timesec"><i class="fa fa-clock-o"></i> 11:00 AM -  
-                                03:00 AM </div>
-                                <div class="address"><i class="fa fa-map-o"></i> New Margania</div>
-                            </div>
-                            </div> 
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 sm-mb-30">
-                    <div class="event-item">
-                        <div class="event-short">
-                            <div class="featured-img">
-                                <img src="{{ asset('frontend')}}/assets/images/event/8.jpg" alt="Image">
-                                <div class="dates">
-                                        July 24, 2020  
-                                </div>
-                            </div>
-                            <div class="content-part">
-                                <h4 class="title"><a href="#">Educavo Learning Gala Day For Kids</a></h4>
-                            <div class="time-sec">
-                                <div class="timesec"><i class="fa fa-clock-o"></i> 11:00 AM -  
-                                03:00 AM </div>
-                                <div class="address"><i class="fa fa-map-o"></i> New Margania</div>
-                            </div>
-                            </div> 
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-12">
-                    <div class="event-item">
-                        <div class="event-short">
-                            <div class="featured-img">
-                                <img src="{{ asset('frontend')}}/assets/images/event/9.jpg" alt="Image">
-                                <div class="dates">
-                                        July 24, 2020  
-                                </div>
-                            </div>
-                            <div class="content-part">
-                                <h4 class="title"><a href="#">Best Technology Graduation Ceremony.</a></h4>
-                            <div class="time-sec">
-                                <div class="timesec"><i class="fa fa-clock-o"></i> 11:00 AM -  
-                                03:00 AM </div>
-                                <div class="address"><i class="fa fa-map-o"></i> New Margania</div>
-                            </div>
-                            </div> 
-                        </div>
-                    </div>
-                </div>
+                @endforeach
             </div>
         </div> 
     </div>
